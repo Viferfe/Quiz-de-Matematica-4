590344f4dee9436f8731adb23c161de9226de5e0
Viferfe <142558392+Viferfe@users.noreply.github.com>
2023-12-17 22:01:42 -0300

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -1,9 +1,9 @@
 function addUser() {
-  player1Name = document.getElementById('player1Name').value
-  player2Name = document.getElementById('player2Name').value
+  player1_name = document.getElementById('player1Name').value
+  player2_name = document.getElementById('player2Name').value
 
-  localStorage.setItem('player1Name', player1Name)
-  localStorage.setItem('player2Name', player2Name)
+  localStorage.setItem('player1Name', player1_name)
+  localStorage.setItem('player2Name', player2_name)
 
   window.location = 'game_page.html'
 }
